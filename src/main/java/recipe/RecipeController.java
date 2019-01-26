ef9c999bfbc3ebc044bbf4d99c9509a513c85782
Andrea Titaro <a.titaro@campus.unimib.it>
2019-01-26 14:34:29 +0100

    Fix some connections

--- src/main/java/recipe/RecipeController.java
+++ src/main/java/recipe/RecipeController.java
@@ -7,11 +7,12 @@ import java.util.logging.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/Recipe")
 public class RecipeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	final Logger logger = Logger.getLogger("MyLog");    
@@ -27,7 +28,7 @@ public class RecipeController extends HttpServlet {
 			List<Recipe> recipes = recipeDao.findAllRecipes();
 			request.setAttribute("recipes", recipes);
 			
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/GlobalRecipe.jsp");
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/recipe/GlobalRecipe.jsp");
 			dispatcher.forward(request, response);
     	}catch(ServletException | IOException e){
 			logger.log(Level.SEVERE, "doGet Servlet error", e);
--- src/main/webapp/recipe/GlobalRecipe.jsp
+++ src/main/webapp/recipe/GlobalRecipe.jsp
@@ -22,7 +22,7 @@
 		<div id="menu">
 			<ul>
 				<li><a href="../index.html" accesskey="1" title="">Homepage</a></li>
-				<li><a href="../TEST/Registration.jsp" accesskey="2" title="">Registrati</a></li>
+				<li><a href="../registration/index.html" accesskey="2" title="">Registrati</a></li>
 				<li class="current_page_item"><a href="#" accesskey="3" title="">Ricette </a></li>
 				<li><a href="#" accesskey="4" title=""></a></li>
 				<li><a href="#" accesskey="5" title=""></a></li>
@@ -42,7 +42,7 @@
 			<span class="byline"><marquee> Le tue birre</span></marquee>
 				<br>
 				<br>
-				#tabella in cui si visualizzano le birre pubbliche (visibili anche dai non registrati) degli utenti
+				
 				<br>
 				<br>
 				<br>
