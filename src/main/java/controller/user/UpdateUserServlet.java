80b9cae67d3e5280f626b8c2160e147623f8d6b9
Andrea Titaro <a.titaro@campus.unimib.it>
2019-02-07 21:33:28 +0100

    Add UpdateUser

---
+++ src/main/java/controller/user/UpdateUserServlet.java
@@ -0,0 +1,75 @@
+package controller.user;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.bouncycastle.util.encoders.Hex;
+
+import model.user.User;
+import model.user.UserDao;
+
+@WebServlet("/updateUser")
+public class UpdateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	final Logger logger = Logger.getLogger("MyLog");
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			HttpSession session = request.getSession(false);
+			if (session != null && session.getAttribute("user") != null) {
+				User user = (User) session.getAttribute("user");
+				int userID = user.getId();
+				String name = request.getParameter("name");
+				String surname = request.getParameter("surname");
+				String date = request.getParameter("dateOfBirth");
+				String oldPassword = request.getParameter("old_password");
+				String password = request.getParameter("new_password");
+				String checkPassword = request.getParameter("confirm_password");
+				String email = request.getParameter("email");
+				UserDao userDao = new UserDao();
+				
+				
+				if (user.getPassword().equals(oldPassword) && password.equals(checkPassword) && userDao.usableEmail(email)) {
+
+					SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest512();
+					byte[] passwordToHash = digestSHA3.digest(password.getBytes());
+					String hash = Hex.toHexString(passwordToHash);
+					Date dateOfBirth;
+					User updateUser;
+
+					try {
+
+						dateOfBirth = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+						updateUser = new User(name, surname, dateOfBirth, email, hash);
+						
+						
+							userDao.updateUser(userID, updateUser);
+					
+							response.sendRedirect("./user");
+						
+
+					} catch (ParseException e) {
+						logger.log(Level.SEVERE, "Parser error", e);
+					}
+			}
+			}
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, "Servlet error", e);
+		}
+
+	}
+	
+}
